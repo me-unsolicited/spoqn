535946e4ba0e9d8ba9f2acca4a8d88beeda5defc
benmannon <benmannon@gmail.com>
2017-04-11 12:00:43 -0400

    Added "id" and "user" properties to /messages resource

--- spoqn-server/src/main/java/com/spoqn/server/core/Messages.java
+++ spoqn-server/src/main/java/com/spoqn/server/core/Messages.java
@@ -3,6 +3,7 @@ package com.spoqn.server.core;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.stereotype.Component;
 
@@ -14,17 +15,20 @@ public class Messages {
     private List<Message> messages = new ArrayList<>();
     {
         // temporary until UI controlled
-        messages.add(message("Hello, World!"));
-        messages.add(message("new phone who dis"));
+        messages.add(message(UUID.randomUUID(), "frodo", "Hello, World!"));
+        messages.add(message(UUID.randomUUID(), "bilbo", "new phone who dis"));
     }
 
-    private Message message(String text) {
+    private Message message(UUID id, String user, String text) {
         Message message = new Message();
+        message.setId(id);
+        message.setUser(user);
         message.setText(text);
         return message;
     }
 
     public Message create(Message message) {
+        message.setId(UUID.randomUUID());
         messages.add(message);
         return message;
     }
--- spoqn-server/src/main/java/com/spoqn/server/data/entities/Message.java
+++ spoqn-server/src/main/java/com/spoqn/server/data/entities/Message.java
@@ -1,8 +1,12 @@
 package com.spoqn.server.data.entities;
 
+import java.util.UUID;
+
 import lombok.Data;
 
 @Data
 public class Message {
+    private UUID id;
+    private String user;
     private String text;
 }
