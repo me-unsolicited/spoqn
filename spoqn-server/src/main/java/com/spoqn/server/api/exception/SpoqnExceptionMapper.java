d2c253ac82b4ecdb76f1619ab54b38df44d29dc3
benmannon <benmannon@gmail.com>
2017-04-12 12:56:33 -0400

    Added log4j dependency to log unknown errors with an incident ID.

--- spoqn-server/src/main/java/com/spoqn/server/api/exception/SpoqnExceptionMapper.java
+++ spoqn-server/src/main/java/com/spoqn/server/api/exception/SpoqnExceptionMapper.java
@@ -1,5 +1,6 @@
 package com.spoqn.server.api.exception;
 
+import java.text.MessageFormat;
 import java.util.UUID;
 
 import javax.ws.rs.core.Response;
@@ -10,15 +11,22 @@ import com.spoqn.server.core.exceptions.SpoqnException;
 import com.spoqn.server.data.entities.CodedError;
 import com.spoqn.server.data.entities.UnknownCodedError;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Provider
+@Slf4j
 public class SpoqnExceptionMapper implements ExceptionMapper<SpoqnException> {
+
+    private static final String MSG_PATTERN = "Incident ID [{0}]";
 
     @Override
     public Response toResponse(SpoqnException exception) {
 
+        // log an incident ID with a stack trace
         UUID incident = UUID.randomUUID();
-        // TODO log stack trace with incident id
+        log.error(MessageFormat.format(MSG_PATTERN, incident), exception);
 
+        // return the incident ID as part of an unknown error response
         CodedError error = new UnknownCodedError(incident);
         return Response.serverError().entity(error).build();
     }
