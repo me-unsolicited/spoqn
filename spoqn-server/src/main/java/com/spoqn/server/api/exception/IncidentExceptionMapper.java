9c67fafc44ab6212e37366d3b5ec6da663c54909
benmannon <benmannon@gmail.com>
2017-04-12 15:15:10 -0400

    Extracted incident error handling into base IncidentExceptionMapper.

---
+++ spoqn-server/src/main/java/com/spoqn/server/api/exception/IncidentExceptionMapper.java
@@ -0,0 +1,30 @@
+package com.spoqn.server.api.exception;
+
+import java.text.MessageFormat;
+import java.util.UUID;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+
+import com.spoqn.server.data.entities.CodedError;
+import com.spoqn.server.data.entities.UnknownCodedError;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public abstract class IncidentExceptionMapper<E extends Throwable> implements ExceptionMapper<E> {
+
+    private static final String MSG_PATTERN = "Incident ID [{0}]";
+
+    @Override
+    public Response toResponse(E exception) {
+
+        // log an incident ID with a stack trace
+        UUID incident = UUID.randomUUID();
+        log.error(MessageFormat.format(MSG_PATTERN, incident), exception);
+
+        // return the incident ID as part of an unknown error response
+        CodedError error = new UnknownCodedError(incident);
+        return Response.serverError().entity(error).build();
+    }
+}
--- spoqn-server/src/main/java/com/spoqn/server/api/exception/ThrowableMapper.java
+++ spoqn-server/src/main/java/com/spoqn/server/api/exception/ThrowableMapper.java
@@ -1,32 +1,7 @@
 package com.spoqn.server.api.exception;
 
-import java.text.MessageFormat;
-import java.util.UUID;
-
-import javax.ws.rs.core.Response;
-import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
 
-import com.spoqn.server.data.entities.CodedError;
-import com.spoqn.server.data.entities.UnknownCodedError;
-
-import lombok.extern.slf4j.Slf4j;
-
 @Provider
-@Slf4j
-public class ThrowableMapper implements ExceptionMapper<Throwable> {
-
-    private static final String MSG_PATTERN = "Incident ID [{0}]";
-
-    @Override
-    public Response toResponse(Throwable exception) {
-
-        // log an incident ID with a stack trace
-        UUID incident = UUID.randomUUID();
-        log.error(MessageFormat.format(MSG_PATTERN, incident), exception);
-
-        // return the incident ID as part of an unknown error response
-        CodedError error = new UnknownCodedError(incident);
-        return Response.serverError().entity(error).build();
-    }
+public class ThrowableMapper extends IncidentExceptionMapper<Throwable> {
 }
