1f042d70c1618fd2e63c10e3ce3118647ed5fcd6
benmannon <benmannon@gmail.com>
2017-04-17 17:12:11 -0400

    Configured mybatis mappers automatically by package scanning.

--- spoqn-server/src/main/java/com/spoqn/server/SpoqnContextListener.java
+++ spoqn-server/src/main/java/com/spoqn/server/SpoqnContextListener.java
@@ -12,7 +12,7 @@ import com.google.inject.Injector;
 import com.google.inject.Module;
 import com.google.inject.servlet.GuiceServletContextListener;
 import com.google.inject.servlet.ServletModule;
-import com.spoqn.server.data.mappers.MessageMapper;
+import com.spoqn.server.data.mappers.MapperHelper;
 import com.squarespace.jersey2.guice.JerseyGuiceModule;
 import com.squarespace.jersey2.guice.JerseyGuiceUtils;
 
@@ -31,7 +31,7 @@ public class SpoqnContextListener extends GuiceServletContextListener {
                 environmentId("development");
                 bindDataSourceProvider(new JndiDataSourceProvider("java:comp/env/dataSource"));
                 bindTransactionFactoryType(JdbcTransactionFactory.class);
-                addMapperClass(MessageMapper.class);
+                addMapperClasses(MapperHelper.pkg());
             }
         });
 
