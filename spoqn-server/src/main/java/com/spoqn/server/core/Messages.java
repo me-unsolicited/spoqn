1b7f1fd2e98062fe9282db8d3126da6d2efea7f2
benmannon <benmannon@gmail.com>
2017-04-17 18:30:04 -0400

    Implemented database-driven messages

--- spoqn-server/src/main/java/com/spoqn/server/core/Messages.java
+++ spoqn-server/src/main/java/com/spoqn/server/core/Messages.java
@@ -1,10 +1,6 @@
 package com.spoqn.server.core;
 
-import java.time.Instant;
-import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.UUID;
 
@@ -17,45 +13,15 @@ import com.spoqn.server.data.mappers.MessageMapper;
 @Singleton
 public class Messages {
 
-    @Inject private Users users;
     @Inject private MessageMapper mapper;
 
-    private HashMap<UUID, Message> messages = new LinkedHashMap<>();
-    {
-        // temporary until UI controlled
-
-        Message message0 = message("frodo", "Frodo", "Hello, World!");
-        Message message1 = message("bilbo", "Bilbo", "new phone who dis");
-
-        messages.put(message0.getId(), message0);
-        messages.put(message1.getId(), message1);
-    }
-
-    private Message message(String user, String displayName, String text) {
-        return Message.builder()
-                .id(UUID.randomUUID())
-                .user(user)
-                .displayName(displayName)
-                .text(text)
-                .timestamp(Instant.now())
-                .build();
-    }
-
     public Message create(String username, Message message) {
-
-        message = message.toBuilder()
-                .id(UUID.randomUUID())
-                .user(username)
-                .displayName(users.read(username).getDisplayName())
-                .timestamp(Instant.now())
-                .build();
-
-        messages.put(message.getId(), message);
-        return message;
+        UUID id = mapper.create(message.toBuilder().user(username).build());
+        return mapper.findOne(id.toString());
     }
 
     public List<Message> read() {
-        return Collections.unmodifiableList(new ArrayList<>(messages.values()));
+        return Collections.unmodifiableList(mapper.findAll());
     }
 
     public Message read(UUID id) {
--- spoqn-server/src/main/java/com/spoqn/server/data/mappers/MessageMapper.java
+++ spoqn-server/src/main/java/com/spoqn/server/data/mappers/MessageMapper.java
@@ -1,8 +1,15 @@
 package com.spoqn.server.data.mappers;
 
+import java.util.List;
+import java.util.UUID;
+
 import com.spoqn.server.data.Message;
 
 public interface MessageMapper {
 
     Message findOne(String id);
+
+    List<Message> findAll();
+
+    UUID create(Message message);
 }
