c38639e5c52db4eeb12767dc82241766d41743fb
benmannon <benmannon@gmail.com>
2017-04-18 22:16:28 -0400

    Added another db changeset with liquibase plugin for testing rollback.

--- spoqn-server/src/main/resources/com/spoqn/server/data/changelog/spoqn.changelog-1.0.sql
+++ spoqn-server/src/main/resources/com/spoqn/server/data/changelog/spoqn.changelog-1.0.sql
@@ -6,3 +6,10 @@ create table replace_me_with_something_real (
     name varchar(255)
 );
 --rollback drop table replace_me_with_something_real;
+
+--changeset bmannon:2
+create table replace_me_with_something_else (
+    id int primary key,
+    name varchar(255)
+);
+--rollback drop table replace_me_with_something_else;
