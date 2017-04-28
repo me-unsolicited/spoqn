465164b7e9d6881f42bf5605bc17e8e165a44e4c
benmannon <benmannon@gmail.com>
2017-04-28 13:49:08 -0400

    Added MessageDao

--- spoqn-server/src/main/java/com/spoqn/server/core/MessageService.java
+++ spoqn-server/src/main/java/com/spoqn/server/core/MessageService.java
@@ -1,47 +1,36 @@
 package com.spoqn.server.core;
 
-import java.nio.charset.StandardCharsets;
-import java.util.Collections;
 import java.util.List;
 import java.util.UUID;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
-import javax.ws.rs.core.MediaType;
 
 import org.mybatis.guice.transactional.Transactional;
 
 import com.spoqn.server.data.Message;
-import com.spoqn.server.data.mappers.MessageMapper;
+import com.spoqn.server.data.access.MessageDao;
 
 @Singleton
 @Transactional
 public class MessageService {
 
-    @Inject private MessageMapper mapper;
+    @Inject private MessageDao dao;
 
     public Message create(String loginId, Message message) {
 
         message = message.toBuilder()
-                .id(UUID.randomUUID())
-                .contentId(UUID.randomUUID())
                 .loginId(loginId)
                 .build();
 
-        // first create content; it will be linked to the message
-        mapper.createContent(message.getContentId(), MediaType.TEXT_PLAIN,
-                message.getContent().getBytes(StandardCharsets.UTF_8));
-
-        // create and return the message itself
-        mapper.create(message);
-        return mapper.findOne(message.getId());
+        return dao.create(message);
     }
 
     public List<Message> read() {
-        return Collections.unmodifiableList(mapper.findAll());
+        return dao.findAll();
     }
 
     public Message read(UUID id) {
-        return mapper.findOne(id);
+        return dao.find(id);
     }
 }
